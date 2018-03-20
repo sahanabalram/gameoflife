11ebd853e05892815b5a0aac76386e6bf17a3a25
Sahana Balram <sahana2008balram@gmail.com>
2018-03-20 16:32:46 -0700

    Remove grid component import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Title from "./components/Title";
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
+      <Title/>
       </div>
     );
   }
