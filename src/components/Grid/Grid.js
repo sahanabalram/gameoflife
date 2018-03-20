785b3f8930a9f639ca507cfcd10e38862540d717
Sahana Balram <sahana2008balram@gmail.com>
2018-03-20 16:47:31 -0700

    Create the grid

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -2,8 +2,23 @@ import React, {Component} from "react";
 
 class Grid extends Component {
     render(){
+        const width = this.props.cols * 14;
+        let rowsArray = [];
+        let boxClass = "";
+        for(let i = 0; i< this.props.rows; i++) {
+            for(let j = 0; j < this.props.cols; j++) {
+                let boxID = i + "_" + j;
+                boxClass = this.props.fullGrid[i][j] ? "box on" : "box off";
+                rowsArray.push(<Box boxClass={boxClass}
+                    key={boxID}
+                    row={i}
+                    col={j}
+                    selectBox={this.props.selectBox}/>
+                )
+            }
+        }
         return(
-            <div>
+            <div className="grid" style={{width:width}}>
             <h1>Grid Component</h1>
             </div>
         )
