814e8176fb2299da921fafe3625de33012ae0e27
Sahana Balram <sahana2008balram@gmail.com>
2018-03-20 16:48:20 -0700

    Pass on properties from grid component

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,17 +1,27 @@
 import React, {Component} from "react";
 import Grid from "../../components/Grid";
+import "./Title.css";
 class Title extends Component {
     constructor() {
         super();
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50;
         this.state = {
-            generation: 0
+            generation: 0,
+            fullGrid: Array(this.rows).fill().map(()=>Array(this.cols).fill(false))
         }
     }
     render() {
         return (
-            <div>
+            <div className="container">
                 <h1>The Game of Life</h1>
-                <Grid/>
+                <Grid 
+                fullGrid={this.state.fullGrid}
+                rows={this.rows}
+                cols={this.cols}
+                selectBox = {this.selectBox}
+                />
                 <h3>Generations: {this.state.generation}</h3>
             </div>
         )
